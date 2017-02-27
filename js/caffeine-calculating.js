cbae16594f4303fea184b3f9fe08cd88b65c7822
Ryan Hodin <ryan951357@gmail.com>
2017-02-27 00:59:57 -0600

    Add a function to add a new row to the table
    ... Also fix a syntax error because I named a variable in an illegal way.
    Oops....

--- js/caffeine-calculating.js
+++ js/caffeine-calculating.js
@@ -1,6 +1,7 @@
-var template-container=document.getElementById("caffeine-table-template-row-container");
+var templateContainer=document.getElementById("caffeine-table-template-row-container");
 var lastAmount=document.getElementById("caffeine-last-amount");
 var lastTime=document.getElementById("caffeine-last-time");
+var table=document.getElementById("caffeine-table");
 
 // First, adjust some of the template values. We'll be cloning it later, so any change made to it will carry over.
 var now=new Date();
@@ -15,14 +16,22 @@ if (minute<10)
 else
 	minute=minute.toString();
 var elt; // The element we're working with at any given time
-elt=template-container.querySelector(".caffeine-table-time").firstChild;
+elt=templateContainer.querySelector(".caffeine-table-time").firstChild;
 elt.max=hour+":"+minute;
 if (parseInt(hour)<8)
 	elt.value=hour+":"+minute;
 
 // Now, add onchange listeners
 
-var elts=template-container.querySelectorAll("select,input");
+var elts=templateContainer.querySelectorAll("select,input");
 for (var i=0; i<elts.length; ++i)
 	elts[i].onchange=handleUpdates();
-	
+
+// Adds a new row to the table by deep-cloning each child of the template and appending the clones to the table
+function addNewRow() {
+	var clone;
+	for (var i=0; i<templateContainer.children.length; ++i) {
+		clone=templateContainer.children[i].cloneNode(true);
+		table.appendChild(clone);
+	}
+}
