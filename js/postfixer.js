24328ed3e6e3697593984bfbd6625f51c5724de8
Ryan Hodin <ryan951357@gmail.com>
2017-03-07 21:41:15 -0600

    Add postfixing code
    I still want to test this and contemplate the error handling before
    marking #6 as fixed.

--- js/postfixer.js
+++ js/postfixer.js
@@ -1,4 +1,22 @@
-function postfix(n)
-{
-	return "th";
+function postfix(n) {
+	var str=n.toString();
+	switch (str.substring(str.length-1)) {
+	case "1":
+		return "st"
+	case "2":
+		return "nd"
+	case "3":
+		return "rd"
+	// All the rest fall through to "th"
+	case "4":
+	case "5":
+	case "6":
+	case "7":
+	case "8":
+	case "9":
+	case "0":
+		return "th"
+	default: // We don't know how to postfix a string that doesn't end in a number.
+		return ""
+	}
 }
